9dd7122a26029d45533d2d75a68271817e87c7b0
Alaa Khalil <alaakhalil093@gmail.com>
2022-01-08 15:17:28 +0100

    About screen and about controller

---
+++ RealEstate/RealEstate/View/Controllers/AboutViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AboutViewController.swift
+//  RealEstate
+//
+//  Created by Alaa Khalil on 06/01/2022.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
