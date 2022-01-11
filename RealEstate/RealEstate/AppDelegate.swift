0868fef2db0411d66c2bd7473f8de54700a0f22a
Alaa Khalil <alaakhalil093@gmail.com>
2022-01-11 20:37:23 +0100

    Setup GoogleMaps

--- RealEstate/RealEstate/AppDelegate.swift
+++ RealEstate/RealEstate/AppDelegate.swift
@@ -6,16 +6,20 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    lazy var window: UIWindow? = UIWindow(frame: UIScreen.main.bounds)
+    let locationService = LocationService()
+    let storyboard = UIStoryboard(name: "Main", bundle: nil)
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+        GMSServices.provideAPIKey("AIzaSyCiwXWqrcuVsUwfrDG33e9eJIVWXfkPP5k")
+     // Override point for customization after application launch.
+     return true
     }
+   
 
     // MARK: UISceneSession Lifecycle
 
@@ -30,7 +34,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
