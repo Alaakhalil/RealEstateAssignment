5eb3ff10b5017025ee71ef88cdebf997145c03e9
Alaa Khalil <alaakhalil093@gmail.com>
2022-01-11 20:25:03 +0100

    Set the houses in housesViewController

--- RealEstate/RealEstate/View/Cells/HouseCell.swift
+++ RealEstate/RealEstate/View/Cells/HouseCell.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Kingfisher
+import CoreLocation
 
 class HouseCell: UITableViewCell {
 
@@ -16,13 +18,49 @@ class HouseCell: UITableViewCell {
     @IBOutlet weak var bedroomsLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
+    let locationService = LocationService()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+    }
+    
+    // setting the house data in the house Cell
+    func setData(house: Houses)-> String{
+        self.sizeLabel.text = "\(house.size ?? 0)"
+        self.addressLabel.text = (house.zip ?? " ") + " " + (house.city ?? " ")
+        self.bathroomsLabel.text = "\(house.bathrooms ?? 0)"
+        self.bedroomsLabel.text = "\(house.bedrooms ?? 0)"
+        
+        // setting the price of the house
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.currencySymbol = "$"
+        formatter.maximumFractionDigits = 0;
+        self.priceLabel.text = formatter.string(from: (house.price ?? 0) as NSNumber)
+        print(formatter.string(from: (house.price ?? 0) as NSNumber)!)
+        
+        //setting the image of house
+        if let value = house.image {
+            if let url = URL(string: (baseUrl + value)) {
+                self.houseImage.setImageWithURL(url: url, placeHolderImage: nil)
+            }
+        }else {
+            self.houseImage.image = nil
+        }
+        // check the location access and determine the distance between current location and house location
+        if (locationService.status == .authorizedWhenInUse) || (locationService.status == .authorizedAlways) {
+            let houseCoordinate = CLLocation(latitude: Double(house.latitude ?? 0), longitude: Double(house.longitude ?? 0))
+            locationService.getLocation()
+            let userCurrentLocation = locationService.userLocation()
+            let distance =  (userCurrentLocation.distance(from:houseCoordinate))/1000
+            distanceLabel.text = String(format:"%.2f km",distance)
+            return String(String(format:"%.2f km",distance))
+        }
+        return " "
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
         
-        // Configure the view for the selected state
     }
 }
--- RealEstate/RealEstate/View/Controllers/HousesViewController.swift
+++ RealEstate/RealEstate/View/Controllers/HousesViewController.swift
@@ -6,33 +6,139 @@
 //
 
 import UIKit
+import CoreLocation
+
+
 
 class HousesViewController: UIViewController {
-
+   
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var housesTableView: UITableView!
+    var searching = false
+    var noDataView = UIView()
+    var distances = [String:String]()
+    var userCoordinate:CLLocation?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.handleNoResults()
+        self.noDataView.isHidden = true
+        self.adjustmentSearchBar()
+        // Setting up refresh UI with proper action to reloadData
+        self.getData()
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        getData()
+    }
+
+    // Handling the Houses and reloaded in the tableView
+    func getData(){
+        HousesViewModel.instance.fetchHouses { status in
+            switch status {
+            case .success: self.housesTableView.reloadData()
+            case .failure: print("Failed to fetch the Data")
+            }
+        }
+    }
+    
+    // Customize searchBar to match the design
+    func adjustmentSearchBar(){
+        self.searchBar.delegate = self
+        searchBar.backgroundImage = UIImage()
+        let searchTextField:UITextField = searchBar.value(forKey: "searchField") as? UITextField ?? UITextField()
+        searchTextField.layer.cornerRadius = 12
+        searchTextField.font = UIFont(name: "GothamSSm-Light", size: 13.0)
+    }
+    
+    //Handling if no results by adding Subview says that no results found
+    func handleNoResults(){
+        let layer = CALayer()
+        let NoResultImage = UIImage(named: "undraw_empty_xct9-2")?.cgImage
+        layer.frame = CGRect(x: 50, y: Int(UIScreen.main.bounds.midY) - 150, width: Int(Float(UIScreen.main.bounds.width) - 100), height: Int(Float(UIScreen.main.bounds.height) / 4))
+        layer.contents = NoResultImage
+        let textLayer = CATextLayer()
+        textLayer.string = "No results found! \n Perhaps try another search?"
+        textLayer.font = UIFont(name: "GothamSSm-Light", size: 16.0)
+        textLayer.fontSize = 16
+        textLayer.foregroundColor =  #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        textLayer.isWrapped = true
+        textLayer.alignmentMode = .center
+        textLayer.frame = CGRect(x: 25, y: Int(UIScreen.main.bounds.midY) + 100 , width: Int(Float(UIScreen.main.bounds.width) - 50), height: Int(Float(UIScreen.main.bounds.height) / 4))
+        noDataView.layer.addSublayer(layer)
+        noDataView.layer.addSublayer(textLayer)
+        self.view.addSubview(noDataView)
+        self.noDataView.isHidden = true
+    }
+    // Handle Refresh Action
+    @objc func handleRefresh(_ refreshControl: UIRefreshControl) {
+        // Do some reloading of data and update the table view's data source
+        print("8888888888888")
+        getData()
+        self.housesTableView.reloadData()
+        refreshControl.endRefreshing()
+    }
+
 }
 
+// MARK: - UITableView
 extension HousesViewController: UITableViewDataSource, UITableViewDelegate{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        if searching {
+            if HousesViewModel.instance.searchedHouses.isEmpty{
+                self.noDataView.isHidden = false
+            }
+            return HousesViewModel.instance.searchedHouses.count
+        } else {
+            self.noDataView.isHidden = true
+            return HousesViewModel.instance.sortedHouses.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = housesTableView.dequeueReusableCell(withIdentifier: "HouseCell") as! HouseCell
+        var houseObject = HousesViewModel.instance.sortedHouses[indexPath.row]
+        if searching{
+            houseObject = HousesViewModel.instance.searchedHouses[indexPath.row]
+        }
+        self.distances["\(houseObject.id ?? 0)"] = cell.setData(house: houseObject)
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let vc = UIStoryboard(name: "HouseDetails", bundle: nil).instantiateViewController(withIdentifier: "HouseDetailController") as! HouseDetailsViewController
+        var selectedHouse = HousesViewModel.instance.sortedHouses[indexPath.row]
+        if searching{
+            selectedHouse = HousesViewModel.instance.searchedHouses[indexPath.row]
+        }
+        let selectedId = selectedHouse.id ?? 0
+        vc.distance = distances["\(selectedId)"]
+        self.searchBar.searchTextField.endEditing(true)
+        vc.houseDetail = selectedHouse
         vc.modalPresentationStyle = .fullScreen
         vc.modalTransitionStyle = .crossDissolve
         self.present(vc, animated: true, completion: nil)
 
     }
+}
+
+// MARK: - UISearchBarDelegate
+extension HousesViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        HousesViewModel.instance.filterContentForSearchText(searchText)
+        if searchText.isEmpty{
+            searching = false
+        }
+        else{
+            searching = true
+        }
+        self.getData()
+    }
     
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searching = false
+        searchBar.text = ""
+        self.getData()
+    }
 }
