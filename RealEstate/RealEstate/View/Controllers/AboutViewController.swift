22252720295c6f025d2adaeb8a57fd7f68525613
Alaa Khalil <alaakhalil093@gmail.com>
2022-01-11 20:25:52 +0100

    AboutViewController

--- RealEstate/RealEstate/View/Controllers/AboutViewController.swift
+++ RealEstate/RealEstate/View/Controllers/AboutViewController.swift
@@ -15,15 +15,12 @@ class AboutViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //Go to the DTT website
+    @IBAction func urlPressedButton(_ sender: Any) {
+        if let url = URL(string: "https://www.d-tt.nl/"),
+           UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url, options: [:])
+        }
     }
-    */
 
 }
