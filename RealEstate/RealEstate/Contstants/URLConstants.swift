c045e362c7d75c922ff55f0c960fe657d6a08c46
Alaa Khalil <alaakhalil093@gmail.com>
2022-01-11 20:17:27 +0100

    Add URLConstrants

---
+++ RealEstate/RealEstate/Contstants/URLConstants.swift
@@ -0,0 +1,16 @@
+//
+//  URLConstants.swift
+//  RealEstate
+//
+//  Created by Alaa Khalil on 08/01/2022.
+//
+
+import Foundation
+import Alamofire
+
+let baseUrl = "https://intern.docker-dev.d-tt.nl/"
+let houseUrl = baseUrl + "api/house/"
+let headers: HTTPHeaders = [
+    "Access-Key": "98bww4ezuzfePCYFxJEWyszbUXc7dxRx"
+]
+
