22a078be432884e9c6c3719e0d442e654ec5d28f
Alaa Khalil <alaakhalil093@gmail.com>
2022-01-12 15:17:41 +0100

    Move all extension files  in extensions group

--- RealEstate/RealEstate/Helpers/Extensions/UIImageViewExtension.swift
+++ RealEstate/RealEstate/Helpers/Extensions/UIImageViewExtension.swift
@@ -9,17 +9,19 @@ import Foundation
 import UIKit
 
 extension UIImageView {
+    
+    // set the image  with url from Api
     func setImageWithURL(url: URL, placeHolderImage placeholder: UIImage?) {
         self.kf.indicatorType = .activity
         self.kf.setImage(with: url,
                          placeholder: placeholder,
                          options: [.transition(.fade(1))],
                          progressBlock: nil) { (result) in
-                            switch result {
-                            case .success(let value):
-                                self.image = value.image
-                            case .failure(let error):
-                                print("KingFisher Job failed: \(error.localizedDescription)")
+            switch result {
+            case .success(let value):
+                self.image = value.image
+            case .failure(let error):
+                print("KingFisher Job failed: \(error.localizedDescription)")
             }
         }
     }
